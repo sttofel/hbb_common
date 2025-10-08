19a22229d04451096ba1c704aed953a68b260231
STTOFEL <filipesttofeL@gmail.com>
2025-10-08 15:47:20 +0100

    Update servers and key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,11 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &[
+    "eu.remote.tksbr.com",
+    "br.remote.tksbr.com"
+    ];
+pub const RS_PUB_KEY: &str = "W3B7SHVB0+7P9pbbk4ca6peUZ54qrM+H0RaeZ81ZgXc=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
